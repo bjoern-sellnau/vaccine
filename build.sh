53f3c9c9836206f2b5924803b39ed00c54e81b03
Jake Sandlund <jtsandlund@gmail.com>
2012-09-14 00:24:29 -0500

    Use module.exports instead of returned value.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 file=test_built.js
 
-echo "(function() {" > $file
-cat test/* >> $file         # Note that vaccine.js is in the test directory.
+echo "(function() {" > $file  # You may want to add a "use strict"; here too
+cat test/* >> $file           # Note that vaccine.js is in the test directory.
 echo "}());" >> $file
 
--- test/test.js
+++ test/test.js
@@ -34,9 +34,9 @@ function verifyCount(lower, upper, func) {
 
   var msg = 'verifyCount failed on function: \n' + func;
 
-  return function(require) {
+  return function(require, exports, module) {
     topCount += 1;
-    var ret = func(require);
+    var ret = func(require, exports, module);
     bottomCount += 1;
 
     if (lower === upper) {
@@ -56,7 +56,7 @@ function verifyCount(lower, upper, func) {
 // Call a single define, as this is needed to set the
 // global _vaccine
 
-define('make the global _vaccine', function() { return true; });
+define('make the global _vaccine', function() {});
 
 assert.equal(_vaccine.get('undef'), undefined);
 
@@ -77,9 +77,9 @@ assert(window.syrup);
 // Don't be using a global define! I'm using it here just to simplify
 // testing.
 
-define('no dependencies', function() {
+define('no dependencies', function(require, exports) {
   window.cool = 'yep';
-  return {returns: 'an object'};
+  exports.returns = 'an object';
 });
 
 assert.equal(window.cool, 'yep');
@@ -88,7 +88,6 @@ assert.equal(window.cool, 'yep');
 define('in order define', function(require) {
   var main = require('no dependencies');
   assert.equal(main.returns, 'an object');
-  return true;
 });
 
 // This function should have actually been called twice,
@@ -98,11 +97,10 @@ define('single out of order define', verifyCount(2, function(require) {
   var defBelow = require('defined below');
 
   assert.equal(defBelow(), 'cool');
-  return true;
 }));
 
-define('defined below', function() {
-  return function() { return 'cool'; };
+define('defined below', function(require, exports, module) {
+  module.exports = function() { return 'cool'; };
 });
 
 
@@ -120,31 +118,30 @@ define('out of order 1', verifyCount(3, 4, function(require) {
   assert.equal(ooo2.phrase, 'pair');
   assert.equal(ooo3.phrase, 'is the magic number');
   assert.equal(ooo5.phrase, 'is a dive');
-  return true;
 }));
 
 // Twice, failing only once for ooo4
-define('out of order 2', verifyCount(2, function(require) {
+define('out of order 2', verifyCount(2, function(require, exports, module) {
   var ooo4 = require('ooo4');
 
   assert.equal(ooo4.phrase, 'of a kind');
-  return {phrase: 'pair'};
+  module.exports = {phrase: 'pair'};
 }));
 
-define('ooo3', verifyCount(2, function(require) {
+define('ooo3', verifyCount(2, function(require, exports) {
   var ooo2 = require('out of order 2');
   assert.equal(ooo2.phrase, 'pair');
-  return {phrase: 'is the magic number'};
+  exports.phrase = 'is the magic number';
 }));
 
-define('ooo4', function(require) {
-  return {phrase: 'of a kind'};
+define('ooo4', function(require, exports) {
+  exports.phrase = 'of a kind';
 });
 
 // Make sure ooo5 triggers ooo1 to define
 assert(!window.ooo1);
-define('ooo5', function(require) {
-  return {phrase: 'is a dive'};
+define('ooo5', function(require, exports) {
+  exports.phrase = 'is a dive';
 });
 assert.equal(window.ooo1, true);
 
@@ -155,12 +152,11 @@ assert.equal(window.ooo1, true);
 try {
   define('exceptional 1', function(require) {
     require('exceptional 2');
-    return true;
   });
 } catch (e) {
   assert(false, 'Should never throw not defined exceptions');
 }
-define('exceptional 2', function() { return true; });
+define('exceptional 2', function() {});
 
 try {
   define('exceptional 3', function(require) {
@@ -175,7 +171,7 @@ try {
 //////////////////////////////////////////////////
 // Testing relative requires.
 
-define('base/one', function() { return 'pajamas'; });
+define('base/one', function(r, e, module) { module.exports = 'pajamas'; });
 
 define('base/two', function(require) {
   var one = require('./one');
@@ -183,7 +179,7 @@ define('base/two', function(require) {
 });
 
 
-define('base/sub/one', function() { return 'night gown'; });
+define('base/sub/one', function(r, e, module) { module.exports = 'night gown'; });
 
 define('base/sub/two', function(require) {
   var one = require('./one');
@@ -199,13 +195,13 @@ assert(window.basicEndSlashWorks);
 
 // Require relative further up
 
-define('not_root', function() { return 'not root'; });
-define('root', function() { return 'root'; });
-define('root/package', function() { return 'root package'; });
-define('root/one', function() { return 'root one'; });
-define('root/one/package', function() { return 'root one package'; });
-define('root/one/two', function() { return 'root .. two'; });
-define('root/one/two/three', function() { return 'root .. three'; });
+define('not_root', function(r, e, module) { module.exports = 'not root'; });
+define('root', function(r, e, module) { module.exports = 'root'; });
+define('root/package', function(r, e, module) { module.exports = 'root package'; });
+define('root/one', function(r, e, module) { module.exports = 'root one'; });
+define('root/one/package', function(r, e, module) { module.exports = 'root one package'; });
+define('root/one/two', function(r, e, module) { module.exports = 'root .. two'; });
+define('root/one/two/three', function(r, e, module) { module.exports = 'root .. three'; });
 
 define('root/one/two/test1', function(require) {
   var three = require('./three'),
