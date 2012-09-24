88c4ac4c1ecbb9adf283dcbc533b2836936b4ed0
Jake Sandlund <jtsandlund@gmail.com>
2012-09-23 21:55:56 -0500

    Small change to remove a 'window.vaccine'.

--- src/vaccine.js
+++ src/vaccine.js
@@ -25,28 +25,27 @@
 >>>>>>>>>>>>>>>>>>>>>>>> LOADER END >>>>>>>>>>>>>>>>>>>>>>>>
   function define(id, defn) {
 
+    var globalVaccine =
 ####################### MINIMAL START #######################
-    if (!window.vaccine) {
+    window.vaccine || (window.vaccine = {
       // The minimal code required to be vaccine compliant.
-      window.vaccine = {
 
-        // w = waiting: Functions to be called when a modules
-        // gets defined. w[moduleId] = [array of functions];
-        w: {},
+      // w = waiting: Functions to be called when a modules
+      // gets defined. w[moduleId] = [array of functions];
+      w: {},
 
-        // m = modules: Modules that have been fully defined.
-        // m[moduleId] = module.exports value
-        m: {},
+      // m = modules: Modules that have been fully defined.
+      // m[moduleId] = module.exports value
+      m: {},
 
-        // s = set: When a module becomes fully defined, set
-        // the module.exports value here.
-        // s(moduleId, module.exports)
-        s: function(id, val) {
-          this.m[id] = val;
-          (this.w[id] || []).forEach(function(w) { w(); });
-        }
-      };
-    }
+      // s = set: When a module becomes fully defined, set
+      // the module.exports value here.
+      // s(moduleId, module.exports)
+      s: function(id, val) {
+        this.m[id] = val;
+        (this.w[id] || []).forEach(function(w) { w(); });
+      }
+    });
     // Set your library with vaccine.s('mylib', mylib);
 >>>>>>>>>>>>>>>>>>>>>>>> MINIMAL END >>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -54,8 +53,7 @@
     var parts = id.split('/');
 >>>>>>>>>>>>>>>>>>>>>>> RELATIVE END >>>>>>>>>>>>>>>>>>>>>>>
 
-    var globalVaccine = window.vaccine,
-        module = {exports: {}};
+    var module = {exports: {}};
 
     function require(reqId) {
 
