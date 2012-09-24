e12c3a6655215d917ead6c9051f8f5ba63dfda78
Jake Sandlund <jtsandlund@gmail.com>
2012-09-24 00:10:36 -0500

    Update root vaccine files.

--- vaccine_minimal.js
+++ vaccine_minimal.js
@@ -1,19 +1,20 @@
-if (!window.vaccine) {
+window.vaccine || (window.vaccine = {
   // The minimal code required to be vaccine compliant.
-  (function() {
-    var waiting = {}, modules = {};
-    window.vaccine = {
-      on: function(id, callback) {
-        (waiting[id] = waiting[id] || []).push(callback);
-      },
-      get: function(id) {
-        return modules[id];
-      },
-      set: function(id, val) {
-        modules[id] = val;
-        (waiting[id] || []).forEach(function(w) { w(); });
-      }
-    };
-  }());
-}
-// Set your library with vaccine.set('mylib', mylib);
+
+  // w = waiting: Functions to be called when a modules
+  // gets defined. w[moduleId] = [array of functions];
+  w: {},
+
+  // m = modules: Modules that have been fully defined.
+  // m[moduleId] = module.exports value
+  m: {},
+
+  // s = set: When a module becomes fully defined, set
+  // the module.exports value here.
+  // s(moduleId, module.exports)
+  s: function(id, val) {
+    this.m[id] = val;
+    (this.w[id] || []).forEach(function(w) { w(); });
+  }
+});
+// Set your library with vaccine.s('mylib', mylib);
