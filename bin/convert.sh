b8b25a7a7b21e3abe3d27f37b688d04fedf76726
Jake Sandlund <jtsandlund@gmail.com>
2012-09-30 18:14:16 -0500

    Safer regex and more functions.

--- bin/convert.sh
+++ bin/convert.sh
@@ -5,13 +5,27 @@ globals=$(cat "$2")
 sources=$(cat "$3")
 
 all_globals=$(echo "$globals")
-all_exports=$(echo "$sources")
-all_requires=$(echo "$sources")
+all_exports=$(echo "$sources" | sed 's/$/:/')
+all_requires=$(echo "$sources" | sed 's/$/:/')
+
+safe_re() {
+  echo "$1" | sed 's/\./\\./g'
+}
+
+append_value() {
+  key=$(safe_re "$1")
+  value=$2
+  sed "s#^$key:.*#&$value:#"
+}
 
 add_to_exports() {
-  add=$1
-  all_exports=$(echo "$all_exports" | sed "s#^$defined_in.*#&:$add#")
+  all_exports=$(echo "$all_exports" | append_value "$defined_in" "$1")
 }
+
+
+# Exports
+
+echo 'Determining Exports'
 
 global_exports=$(grep "\<$global\>[.[:alnum:]]* *=" $sources |
                  sed -e 's/ *=.*//' -e "s/:.*$global/:$global/")
@@ -33,13 +47,18 @@ do
   add_to_exports $global
 done
 
-all_exports=$(echo "$all_exports" | sed 's/$/:/')
 all_globals="$(echo "$all_globals" | sort | uniq)"
+
+
+# Requires
+
+echo ''
+echo 'Determining Requires'
 
 for global in $all_globals
 do
   printf '.'
-  global_re=$(echo "$global" | sed 's/\./\\./g')
+  global_re=$(safe_re "$global")
   defined_in=$(echo "$all_exports" | grep ":$global_re:" | cut -d ':' -f 1)
   num=$(echo "$defined_in" | wc -l | sed 's/ //g')
   if test "$num" -gt 1
@@ -52,9 +71,10 @@ do
   requires=$(grep "\<$global[^.[:alnum:]]" $sources | sed 1d | cut -d ':' -f 1)
   for requiring in $requires
   do
-    if ! $(echo "$all_requires" | grep -q "^$requiring.*:$defined_in")
+    requiring_re=$(safe_re "$requiring")
+    if ! $(echo "$all_requires" | grep -q "^$requiring_re.*:$defined_in")
     then
-      all_requires=$(echo "$all_requires" | sed "s#^$requiring.*#&:$defined_in#")
+      all_requires=$(echo "$all_requires" | append_value "$requiring" "$defined_in")
     fi
   done
 done
