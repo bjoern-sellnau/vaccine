9614569baa03d199bf66c796d09db431e6469ba2
Jake Sandlund <jtsandlund@gmail.com>
2012-10-01 01:46:26 -0500

    Make sure require var is right, and warn if conflicting.

--- bin/convert.sh
+++ bin/convert.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+warn() {
+  echo "$1" >&2
+}
+
 app_name=$1
 global=$2
 source_dir=$3
@@ -8,6 +12,7 @@ sources=$(cat "$5")
 
 all_globals=_vaccine_all_globals
 all_exports=_vaccine_all_exports
+all_module_exports=_vaccine_all_module_exports
 all_requires=_vaccine_all_requires
 all_pullouts=_vaccine_all_pullouts
 all_require_vars=_vaccine_all_require_vars
@@ -47,6 +52,9 @@ done
 sort_uniq all_globals
 sort_uniq all_exports
 
+# Module.exports
+sed 's/:.*//' $all_exports | uniq -u > $all_module_exports
+
 
 # Requires
 
@@ -55,20 +63,34 @@ do
   global_re=$(safe_re "$global")
   defined_in=$(grep ":$global_re$" $all_exports | cut -d ':' -f 1)
   num=$(echo "$defined_in" | wc -l | sed 's/ //g')
-  if test "$num" -gt 1
+  if test "$num" -ne 1
   then
-    echo '' >&2
-    echo "$global defined $num times. Using first definition:" >&2
-    echo "$defined_in" | sed 's/^/> /' >&2
+    warn "$global defined $num times. Using first definition:"
+    warn $(echo "$defined_in" | sed 's/^/> /')
     defined_in=$(echo "$defined_in" | head -n 1)
   fi
   def_module=$(echo "$defined_in" | sed -e 's/\/index.js//' \
                -e 's/\.js$//' -e "s#:$source_dir/#:#" -e "s#:#:$app_name/#")
   defined_in_re=$(safe_re "$defined_in")
-  requires=$(grep "\<$global[^.[:alnum:]]" $sources | sed 1d | cut -d ':' -f 1 |
-             grep -v "^$defined_in_re$" | sed "s#\$#:$def_module#")
+  if grep -q "^$defined_in_re$" $all_module_exports
+  then
+    module_exports=true
+    var=$global
+  else
+    module_exports=false
+    var=$(echo "$def_module" | sed 's/.*\///')
+    if test "X$var" = "X$global"
+    then
+      warn "$global (defined in $defined_in) conflicts with require var name"
+    fi
+  fi
+  requires=$(grep "\<$global[^.[:alnum:]]" $sources | cut -d ':' -f 1 |
+             grep -v "^$defined_in_re$" | sed "s#\$#:$var:$def_module#")
   echo "$requires" >> $all_requires
-  echo "$requires" | sed "s/$/%$global/" >> $all_pullouts
+  if test "X$module_exports" != Xtrue
+  then
+    echo "$requires" | sed "s/$/:$global/" >> $all_pullouts
+  fi
 done
 
 sort_uniq all_requires
@@ -77,10 +99,10 @@ sort_uniq all_pullouts
 
 # Vars
 
-sed -e "s/:\(.*\)/& = require('\1'),/" \
-    -e 's#:[^=]*/#:    #' $all_requires > $all_require_vars
+sed -e "s/:\(.*\):\(.*\)$/:    \1 = require('\2'),/" \
+    $all_requires > $all_require_vars
 
-sed 's#:.*/\(.*\)%\(.*\)#:    \2 = \1.\2,#' $all_pullouts > $all_pullout_vars
+sed 's/:\(.*\):.*:\(.*\)/:    \2 = \1.\2,/' $all_pullouts > $all_pullout_vars
 
 
 # Write to files
