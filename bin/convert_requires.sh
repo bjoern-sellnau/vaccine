0fd5a8a66f6d8a9c560f74ce5cc300b7c110d6bb
Jake Sandlund <jtsandlund@gmail.com>
2012-10-02 00:49:50 -0500

    Convert --to absolute.

--- bin/convert_requires.sh
+++ bin/convert_requires.sh
@@ -2,34 +2,50 @@ convert_to=$1
 app_name=$2
 source_dir=$3
 
+safe_re() {
+  echo "$1" | sed 's/\./\\./g'
+}
+
+get_module() {
+  echo "$source" | sed -e "s#^$source_dir/##" -e "s#^#$app_name/#" \
+                       -e 's/\.js$//' -e 's#/$##'
+}
+
 pop_dir() {
   current=$1
   if ! $(echo "$current" | grep -q '/')
   then
-    printf '.'
+    printf ''
   else
     echo $current | sed 's#/[^/]*$##'
   fi
 }
 
-make_relative() {
-  remove_dir=$1
-  back=$2
+switch_relative() {
+  from=$1
+  to=$2
   source=$3
-  if test "X$remove_dir" != 'X.'
+  if test "X$from" != X -a "X$to" != X
   then
-    sed -i '' -e "s#require(\(.\)$remove_dir[[:>:]]#require(\1$back#" "$source"
+    from_re=$(safe_re "$from")
+    sed -i '' -e "s#require(\(.\)$from_re\([\"'/]\)#require(\1$to\2#" "$source"
   fi
 }
 
 for source in $(find "$source_dir" -type f)
 do
-  module=$(echo "$source" | sed -e "s#^$source_dir/##" -e "s#^#$app_name/#" \
-                                -e 's/\.js$//' -e 's#/$##')
+  module=$(get_module "$source")
   same_dir=$(pop_dir $module)
-  make_relative $same_dir '.' "$source" $module
   back_one_dir=$(pop_dir $same_dir)
-  make_relative $back_one_dir '..' "$source" $module
   back_two_dirs=$(pop_dir $back_one_dir)
-  make_relative $back_two_dirs '../..' "$source" $module
+  if test "X$convert_to" = Xrelative
+  then
+    switch_relative "$same_dir" '.' "$source"
+    switch_relative "$back_one_dir" '..' "$source"
+    switch_relative "$back_two_dirs" '../..' "$source"
+  else
+    switch_relative '../..' "$back_two_dirs" "$source"
+    switch_relative '..' "$back_one_dir" "$source"
+    switch_relative '.' "$same_dir" "$source"
+  fi
 done
