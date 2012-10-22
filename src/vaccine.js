e345eaba11adace74777f2f6674bc3638ce9f21c
Jake Sandlund <jtsandlund@gmail.com>
2012-10-22 15:49:09 -0500

    Add indentation to source vaccine.

--- src/vaccine.js
+++ src/vaccine.js
@@ -1,93 +1,93 @@
-  function define(id, factory) {
-    (vaccineFactories = vaccineFactories || {})[
+    function define(id, factory) {
+      (vaccineFactories = vaccineFactories || {})[
 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SIMPLE
-      './' + id
+        './' + id
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SIMPLE
 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv COMPLEX
-      id
+        id
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ COMPLEX
-    ] = factory;
-  }
-
-
-  function require(id) {
-
-    var
-vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv COMPLEX
-        parts = id.split('/'),
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ COMPLEX
-        module = {exports: {}};
-
-    if (!vaccineLocalModules[id] && !vaccineWindow[id]) {
-      vaccineFactories[id](
-vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SIMPLE
-          require,
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SIMPLE
-vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv COMPLEX
-          function(reqId) {
-            var matching = /(\.?\.\/?)*/.exec(reqId)[0],
-                // Some code golf to get the number of "directories" back we want to go
-                back = Math.floor(matching.replace(/\//g, '').length / 1.9 + 0.99),
-                base;
-            if (back) {
-              base = parts.slice(0, parts.length - back).join('/');
-              if (base) base += '/';
-              reqId = base + reqId.slice(matching.length);
-            }
-            return require(reqId.replace(/\/$/, ''));
-          },
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ COMPLEX
-          module.exports, module);
-      vaccineLocalModules[id] = module.exports;
+      ] = factory;
     }
-    return vaccineLocalModules[id] || vaccineWindow[id];
-  }
 
 
-  var vaccineFactories,
-      vaccineLocalModules = {},
+    function require(id) {
+
+      var
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv COMPLEX
+          parts = id.split('/'),
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ COMPLEX
+          module = {exports: {}};
+
+      if (!vaccineLocalModules[id] && !vaccineWindow[id]) {
+        vaccineFactories[id](
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SIMPLE
+            require,
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SIMPLE
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv COMPLEX
+            function(reqId) {
+              var matching = /(\.?\.\/?)*/.exec(reqId)[0],
+                  // Some code golf to get the number of "directories" back we want to go
+                  back = Math.floor(matching.replace(/\//g, '').length / 1.9 + 0.99),
+                  base;
+              if (back) {
+                base = parts.slice(0, parts.length - back).join('/');
+                if (base) base += '/';
+                reqId = base + reqId.slice(matching.length);
+              }
+              return require(reqId.replace(/\/$/, ''));
+            },
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ COMPLEX
+            module.exports, module);
+        vaccineLocalModules[id] = module.exports;
+      }
+      return vaccineLocalModules[id] || vaccineWindow[id];
+    }
+
+
+    var vaccineFactories,
+        vaccineLocalModules = {},
 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv MULTIPLE_DEPS
-      vaccineDependencies = [{{{ DEP_NAMES }}}],
+        vaccineDependencies = [{{{ DEP_NAMES }}}],
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ MULTIPLE_DEPS
-      vaccineWindow = window;
+        vaccineWindow = window;
 
 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SET_APP
  vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv AMD
-  if (typeof vaccineWindow.define == 'function' &&
-      vaccineWindow.define.amd) {
-    define('{{{ APP_NAME }}}',
+    if (typeof vaccineWindow.define == 'function' &&
+        vaccineWindow.define.amd) {
+      define('{{{ APP_NAME }}}',
   vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SINGLE_DEP
-        [{{{ DEP_NAMES }}}],
-        function(vaccineSingleDep) {
-          vaccineLocalModules.{{{ DEP_NAME }}} = vaccineSingleDep;
+          [{{{ DEP_NAMES }}}],
+          function(vaccineSingleDep) {
+            vaccineLocalModules.{{{ DEP_NAME }}} = vaccineSingleDep;
   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SINGLE_DEP
   vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv MULTIPLE_DEPS
-        vaccineDependencies,
-        function() {
-          for (var i = 0, args = arguments; i < args.length; ++i) {
-            vaccineLocalModules[vaccineDependencies[i]] = args[i];
-          }
+          vaccineDependencies,
+          function() {
+            for (var i = 0, args = arguments; i < args.length; ++i) {
+              vaccineLocalModules[vaccineDependencies[i]] = args[i];
+            }
   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ MULTIPLE_DEPS
   vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv NO_DEPS
-        function() {
+          function() {
   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ NO_DEPS
   vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SIMPLE
-          return require('./index');
+            return require('./index');
   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SIMPLE
   vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv COMPLEX
-          return require('index');
+            return require('index');
   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ COMPLEX
-        });
-  } else {
+          });
+    } else {
  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AMD
-    vaccineWindow.{{{ APP_NAME }}} =
- vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SIMPLE
-        require('./index');
- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SIMPLE
- vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv COMPLEX
-        require('index');
- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ COMPLEX
- vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv AMD
-  }
- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AMD
+      vaccineWindow.{{{ APP_NAME }}} =
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SIMPLE
+          require('./index');
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SIMPLE
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv COMPLEX
+          require('index');
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ COMPLEX
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv AMD
+    }
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AMD
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SET_APP
