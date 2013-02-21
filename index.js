952a1d7f804ab77c377c089826a64039025a2f34
Jake Sandlund <jtsandlund@gmail.com>
2013-02-20 20:35:42 -0600

    targets command working.

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var fs = require('fs'),
 var templateFiles = ['vaccine.js', 'Makefile', 'build.sh', 'dev_server.js',
                      'umd.js'];
 var templateText = {};
+var componentOptions;
+
+var fail = function(message, num) {
+  console.error(message);
+  process.exit(num || 1);
+};
 
 var optionLocations = {
   format: 'vaccine.format',
@@ -36,12 +42,20 @@ var optionDefaults = {
 };
 
 
-var actions = {
+module.exports = exports = {
+  defaultForFormat: vaccine.defaultForFormat,
+  validateOptions: vaccine.validateOptions,
+
+  templateText: function(_) {
+    if (!_) return templateText;
+    templateText = _;
+  },
+
   targets: function(targets) {
-    var options = loadOptions();
-    console.log(options);
-    process.exit(0);
-    targets.forEach(function(target) {
+    if (!targets || !targets.length)
+      targets = loadOptions().targets;
+    var compiled = compileTargets(targets);
+    compiled.forEach(function(target) {
       var name = target.name;
       if (name === 'Makefile') {
         if (fs.existsSync('Makefile')) name = 'Makefile.example';
@@ -56,22 +70,48 @@ var actions = {
     });
   },
 };
-module.exports = exports = actions;
 
-var loadTemplates = function() {
-  var fs = require('fs');
-  templateFiles.forEach(function(file) {
-    templateText[file] = fs.readFileSync(__dirname + '/../templates/' + file, 'utf8');
-  });
+var compileTargets = function(targets, options) {
+  var options = options || loadOptions();
+  options = clone(options);
+  options.targets = targets;
+  return compile(options);
+};
+exports.compileTargets = compileTargets;
+
+var compile = function(options) {
+  var options = options || loadOptions();
+  var problems = vaccine.validateOptions(options);
+  if (problems.length) {
+    problems.forEach(function(problem) {
+      // TODO: log problems.
+    });
+    console.log(problems);
+    console.log(problems[0].options);
+    fail("Problem with options");
+  }
+  return vaccine(options);
+};
+exports.compile = compile;
+
+var clone = function(object) {
+  var copy = {};
+  var i;
+  for (i in object)
+    if (object.hasOwnProperty(i))
+      copy[i] = object[i];
+  return copy;
 };
 
 var loadOptions = function() {
+  if (componentOptions) return componentOptions;
   var jsonFile = 'vaccine.json';
   if (!fs.existsSync(jsonFile)) jsonFile = 'component.json';
   if (!fs.existsSync(jsonFile))
     fail("Must specify options in component.json (vaccine.json for apps)");
   var component = JSON.parse(fs.readFileSync(jsonFile));
-  return determineOptions(component);
+  componentOptions = determineOptions(component);
+  return componentOptions;
 };
 
 var determineOptions = function(component) {
@@ -99,3 +139,13 @@ var determineOptions = function(component) {
   });
   return options;
 };
+
+var loadTemplates = function() {
+  var fs = require('fs');
+  templateFiles.forEach(function(file) {
+    templateText[file] = fs.readFileSync(__dirname + '/templates/' + file, 'utf8');
+  });
+  vaccine.templateText(templateText);
+};
+
+loadTemplates();
